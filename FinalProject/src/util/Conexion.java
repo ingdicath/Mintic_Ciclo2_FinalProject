958c412567d2814c630b099eb2f986714d3180f0
Diana Salamanca <dsalaman@student.codam.nl>
2021-08-27 17:05:15 +0200

    Upload controller, model Propietario, conexion and properties

---
+++ FinalProject/src/util/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ * Connection setup with the sql DB.
+ *
+ * @author diani
+ */
+public class Conexion {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    Properties.dbURL, Properties.username, Properties.password);
+            if (conn != null) {
+                System.out.println("*** Connected to database ***");
+                return conn;
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Código : "
+                    + ex.getErrorCode() + "\nError :" + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+
+}
---
+++ FinalProject/src/util/Properties.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ * sql DB properties setup. Please change username, password and db URL
+ * accordingly.
+ *
+ * @author diani
+ */
+public interface Properties {
+
+    String username = "root";
+    String password = "";
+    String dbURL = "jdbc:mysql://localhost:3306/veterinariaPoopis";
+}
